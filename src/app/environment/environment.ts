e69eda9b31244ef19145bad8cd842745749e1dbb
Bikash Shah <bikash.shah@f1soft.com>
2024-09-06 21:02:49 +0545

    en file changed

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    WEB_ENDPOINT:'http://localhost:5555'
+    WEB_ENDPOINT:'https://causal-jody-zbytes-45f76556.koyeb.app'
+    //WEB_ENDPOINT:'fearless-strength2.up.railway.app'
 }
